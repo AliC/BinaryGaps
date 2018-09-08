cbe250a9368fb52d9527444a4a6f3a639c5dc1a9
Alastair Danley-Cox <AliC@users.noreply.github.com>
2018-09-08 14:21:01 +0100

    Improved test names, amended first test

---
+++ BinaryGaps/LongestGapTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace BinaryGaps
+{
+    public class LongestGapTests
+    {
+        [Theory]
+        [InlineData(1,0)]
+        public void Binary_Gaps_Are_Detected_Correctly(int positiveInteger, int expectedLongestGap)
+        {
+            var longestGapFinder = new Solution();
+            var actualLongestGap = longestGapFinder.solution(positiveInteger);
+
+            Assert.Equal(expectedLongestGap, actualLongestGap);
+        }
+    }
+}
